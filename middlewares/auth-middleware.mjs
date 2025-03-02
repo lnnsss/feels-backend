92e9d225d52ba54f5e7005f05cc8e70723242f8c
lns <bezborodnikovtimur@gmail.com>
2025-03-02 19:19:51 +0300

    name validation fixed

--- middlewares/auth-middleware.mjs
+++ middlewares/auth-middleware.mjs
@@ -2,47 +2,53 @@ import { body } from "express-validator";
 
 export const registerValidation = [
   body("email")
-    .isEmail()
-    .withMessage("Неверный формат электронной почты")
-    .not()
-    .contains(" ")
-    .withMessage("Электронная почта не должна содержать пробелов"),
+      .isEmail()
+      .withMessage("Неверный формат электронной почты")
+      .not()
+      .contains(" ")
+      .withMessage("Электронная почта не должна содержать пробелов"),
 
   body("password")
-    .isLength({ min: 6 })
-    .withMessage("Пароль должен содержать не меньше 6 символов")
-    .matches(/[a-z]/)
-    .withMessage("Пароль должен содержать строчные буквы")
-    .matches(/[A-Z]/)
-    .withMessage("Пароль должен содержать заглавные буквы")
-    .matches(/[!#$%&?]/)
-    .withMessage("Пароль должен содержать специальные символы (! # $ % & ?)")
-    .not()
-    .matches(/[а-яА-Я]/)
-    .withMessage("Пароль не должен содержать русские буквы")
-    .not()
-    .contains(" ")
-    .withMessage("Пароль не должен содержать пробелов"),
+      .isLength({ min: 6 })
+      .withMessage("Пароль должен содержать не меньше 6 символов")
+      .matches(/[a-z]/)
+      .withMessage("Пароль должен содержать строчные буквы")
+      .matches(/[A-Z]/)
+      .withMessage("Пароль должен содержать заглавные буквы")
+      .matches(/[!#$%&?]/)
+      .withMessage("Пароль должен содержать специальные символы (! # $ % & ?)")
+      .not()
+      .matches(/[а-яА-Я]/)
+      .withMessage("Пароль не должен содержать русские буквы")
+      .not()
+      .contains(" ")
+      .withMessage("Пароль не должен содержать пробелов"),
 
   body("name")
-    .isLength({ max: 14 })
-    .withMessage("Имя не должно быть длиннее 14 символов")
-    .not()
-    .contains(" ")
-    .withMessage("Имя не должно содержать пробелов")
-    .not()
-    .matches(/[!#$%&?]/)
-    .withMessage("Имя не может содержать специальные символы (! # $ % & ?)"),
+      .isLength({ max: 14 })
+      .withMessage("Имя не должно быть длиннее 14 символов")
+      .not()
+      .contains(" ")
+      .withMessage("Имя не должно содержать пробелов")
+      .not()
+      .matches(/[!#$%&?]/)
+      .withMessage("Имя не может содержать специальные символы (! # $ % & ?)")
+      .not()
+      .matches(/\d/)
+      .withMessage("Имя не может содержать цифры"),
 
   body("lastName")
-    .isLength({ max: 20 })
-    .withMessage("Фамилия не должна быть длиннее 20 символов")
-    .not()
-    .contains(" ")
-    .withMessage("Фамилия не должна содержать пробелов")
-    .not()
-    .matches(/[!#$%&?]/)
-    .withMessage(
-      "Фамилия не может содержать специальные символы (! # $ % & ?)"
-    ),
+      .isLength({ max: 20 })
+      .withMessage("Фамилия не должна быть длиннее 20 символов")
+      .not()
+      .contains(" ")
+      .withMessage("Фамилия не должна содержать пробелов")
+      .not()
+      .matches(/[!#$%&?]/)
+      .withMessage(
+          "Фамилия не может содержать специальные символы (! # $ % & ?)"
+      )
+      .not()
+      .matches(/\d/)
+      .withMessage("Фамилия не может содержать цифры"),
 ];
--- middlewares/edit-middleware.mjs
+++ middlewares/edit-middleware.mjs
@@ -1,6 +1,6 @@
 import { body } from "express-validator";
 
-// Проверки, что URL ведет на изображение
+// Проверка, что URL ведет на изображение
 const isImageUrl = (value) => {
   if (!value) return true;
 
@@ -15,46 +15,52 @@ const isImageUrl = (value) => {
 
 export const editValidation = [
   body("name")
-    .optional()
-    .isLength({ max: 14 })
-    .withMessage("Имя не должно быть длиннее 14 символов")
-    .not()
-    .matches(/[!#$%&?]/)
-    .withMessage("Имя не может содержать специальные символы (! # $ % & ?)")
-    .not()
-    .contains(" ")
-    .withMessage("Имя не должно содержать пробелов"), 
+      .optional()
+      .isLength({ max: 14 })
+      .withMessage("Имя не должно быть длиннее 14 символов")
+      .not()
+      .matches(/[!#$%&?]/)
+      .withMessage("Имя не может содержать специальные символы (! # $ % & ?)")
+      .not()
+      .contains(" ")
+      .withMessage("Имя не должно содержать пробелов")
+      .not()
+      .matches(/\d/)
+      .withMessage("Имя не может содержать цифры"),
 
   body("lastName")
-    .optional()
-    .isLength({ max: 20 })
-    .withMessage("Фамилия не должна быть длиннее 20 символов")
-    .not()
-    .matches(/[!#$%&?]/)
-    .withMessage("Фамилия не может содержать специальные символы (! # $ % & ?)")
-    .not()
-    .contains(" ")
-    .withMessage("Фамилия не должна содержать пробелов"), 
+      .optional()
+      .isLength({ max: 20 })
+      .withMessage("Фамилия не должна быть длиннее 20 символов")
+      .not()
+      .matches(/[!#$%&?]/)
+      .withMessage("Фамилия не может содержать специальные символы (! # $ % & ?)")
+      .not()
+      .contains(" ")
+      .withMessage("Фамилия не должна содержать пробелов")
+      .not()
+      .matches(/\d/)
+      .withMessage("Фамилия не может содержать цифры"),
 
   body("userName")
-    .optional()
-    .isLength({ min: 3, max: 15 })
-    .withMessage("Имя пользователя должно быть от 3 до 15 символов")
-    .matches(/^[A-Za-z0-9]+$/)
-    .withMessage("Юзернейм может содержать только английские буквы и цифры")
-    .matches(/[A-Za-z]/)
-    .withMessage("Юзернейм должен содержать хотя бы одну букву")
-    .not()
-    .contains(" ")
-    .withMessage("Юзернейм не должен содержать пробелов"),
+      .optional()
+      .isLength({ min: 3, max: 15 })
+      .withMessage("Имя пользователя должно быть от 3 до 15 символов")
+      .matches(/^[A-Za-z0-9]+$/)
+      .withMessage("Юзернейм может содержать только английские буквы и цифры")
+      .matches(/[A-Za-z]/)
+      .withMessage("Юзернейм должен содержать хотя бы одну букву")
+      .not()
+      .contains(" ")
+      .withMessage("Юзернейм не должен содержать пробелов"),
 
   body("status")
-    .optional()
-    .isLength({ max: 30 })
-    .withMessage("Статус не должен быть длиннее 30 символов"),
+      .optional()
+      .isLength({ max: 30 })
+      .withMessage("Статус не должен быть длиннее 30 символов"),
 
   body("avatarURL")
-    .optional()
-    .custom(isImageUrl)
-    .withMessage("URL аватарки должен быть корректной ссылкой на изображение"),
+      .optional()
+      .custom(isImageUrl)
+      .withMessage("URL аватарки должен быть корректной ссылкой на изображение"),
 ];
