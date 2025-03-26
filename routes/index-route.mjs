e514d408cb221f1d2e98cbfaa35c1469f2c5c87e
lns <142323634+lnnsss@users.noreply.github.com>
2025-03-26 12:30:42 +0300

    Update index-route.mjs

--- routes/index-route.mjs
+++ routes/index-route.mjs
@@ -2,6 +2,7 @@ import { Router } from "express";
 import AuthRoutes from "./auth-route.mjs";
 import UserRoutes from "./user-route.mjs";
 import PostRoutes from "./post-route.mjs";
+import ChatRoutes from "./chat-route.mjs"
 
 const router = Router();
 
@@ -13,5 +14,8 @@ router.use("/users", UserRoutes);
 
 // Посты
 router.use("/posts", PostRoutes);
+
+// Чаты
+router.use("/chats", ChatRoutes);
 
 export default router;
