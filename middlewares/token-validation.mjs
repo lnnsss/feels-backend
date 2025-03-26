9d4c6355e8568ce4bb97eda8f435d49f99036c12
lns <142323634+lnnsss@users.noreply.github.com>
2025-03-26 12:31:33 +0300

    Create token-validation.mjs

---
+++ middlewares/token-validation.mjs
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+const secret = process.env.JWT_SECRET;
+
+const tokenValidation = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({ message: "Нет доступа" });
+    }
+
+    const decoded = jwt.verify(token, secret);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Недействительный токен" });
+  }
+};
+
+export default tokenValidation;
